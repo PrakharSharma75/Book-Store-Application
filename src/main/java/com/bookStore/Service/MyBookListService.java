f884949ceaadd24cc151acb891a01adcb9be967e
PrakharSharma75 <proprakhar.sharma@gmail.com>
2024-07-02 11:06:00 +0530

    Done

---
+++ src/main/java/com/bookStore/Service/MyBookListService.java
@@ -0,0 +1,32 @@
+package com.bookStore.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookStore.Entity.MyBookList;
+import com.bookStore.Repository.MyBookRepository;
+
+@Service
+public class MyBookListService 
+{
+	@Autowired
+	private MyBookRepository mybook;
+	
+   public void saveMyBooks(MyBookList book)
+   {
+	   mybook.save(book);
+   }
+   
+   public List<MyBookList> getAllMyBooks()
+   {
+	   return mybook.findAll();
+   }
+   
+   public void deleteById(int id)
+   {
+	   mybook.deleteById(id);
+   }
+   
+}
